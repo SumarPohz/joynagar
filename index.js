e9a4411792209151394e0fb1de98af94117a98c2
SumarPohz <sumarpohz@gmail.com>
2023-07-25 23:19:34 +0530

    updated

--- index.js
+++
@@ -1,9 +0,0 @@
-function showThankYou() {
-    alert("Thank you, submitted successfully.");
-  }
-
-  function submitForm() {
-    var form = document.getElementById("meetingForm");
-    form.submit();
-    showThankYou();
-  }
